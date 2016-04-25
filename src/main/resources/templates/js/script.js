fb1c48bbb178194cade9722b31e722d1a154c68f
Petr <petr.koucky@compass-com.eu>
2016-04-25 12:10:43 +0200

    Final work

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,92 +1,227 @@
-var TODOApp = angular.module('TODOApp', ['TODOAppControllers']);
+var GameOfLife = angular.module('GameOfLife', ['Controllers']);
 
-TODOApp.constant("apiURL", "backend/backend.php");
-
-TODOApp.config(['$httpProvider', function($httpProvider) {
+GameOfLife.config(['$httpProvider', function($httpProvider) {
 	$httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 }]);
 
-TODOApp.run(['$rootScope', function($rootScope) {
-	$rootScope.currentTask = null;
-	$rootScope.tasks = [];
+GameOfLife.run(['$rootScope', function($rootScope) {
 }]);
 
 //------------------------------------------------------------------------------
 
-var TODOAppControllers = angular.module('TODOAppControllers', []);
+var Controllers = angular.module('Controllers', []);
 
-TODOAppControllers.controller("TasksCtrl", ["$http", "$rootScope", "$scope", "apiURL", function($http, $rootScope, $scope, apiURL) {
-	console.log("TasksCtrl");
-  
-  $scope.pozdrav = "Hello Tam kam je ptaci mohou!";
-  $http.get(apiURL).success(function(data) {
-     if(data.success) {
-      	$rootScope.tasks = data.tasks; 
-     }
-  });
-  
-  $scope.showTask = function(t) {
-    var ct = $rootScope.currentTask;
-    if (ct && ct.id == t.id) {                    // odznacuju oznaceny ukol
-      $rootScope.currentTask = null;
-    } else {                                      // oznacuju ukol
-      $rootScope.currentTask = t;
+Controllers.controller("GameOfLife", ["$http", "$rootScope", "$scope", "$interval", function($http, $rootScope, $scope, $interval) {
+  $scope.height = 20;
+  $scope.width = 30;
+  $scope.runnerInterval = 100;
+
+  $scope.newGame = function (pattern) {
+    $scope.history = [];
+
+    switch(pattern) {
+      case 1:
+        $scope.board = init($scope.height, $scope.width);
+        break;
+      case 2:
+        $scope.board = pattern1();
+        break;
+      default:
+        $scope.board = init($scope.height, $scope.width);
     }
   };
-  
-  $scope.deleteTask = function(t) {
-    var param = "delete_id=" + t.id;
-    $http.post(apiURL, param).success(function(data) {
-      if(data.success) {
-        var i = $rootScope.tasks.indexOf(t);
-        if(i != -1) {
-          $rootScope.tasks.splice(i, 1);  // splices
-        }
-      }
-    });
+
+  $scope.updateGame = function(pattern) {
+    $scope.newGame(pattern);
   };
 
-}]);
+  $scope.next = function () {
+    $scope.history.push($scope.board);
+    $scope.board = computeNext($scope.board);
+  };
 
-TODOAppControllers.controller("NewTaskCtrl", ["$http", "$rootScope", "$scope", "apiURL", function($http, $rootScope, $scope, apiURL) {
-	console.log("NewTaskCtrl");
-  $scope.tile = "";
-  $scope.description = "";
-  
-  $scope.addTask = function() {
-    console.log("SPUSTILA SA KONSOLA");
-    
-    if($scope.title) {
-    var param = "title=" + $scope.title + "&description=" + $scope.description; //& je rozdeleni 
-    
-      $http.post(apiURL, param).success(function(data) {
-         if(data.success) {
-            $rootScope.tasks.push(data.task);
-         }
-      });
-    } else {
-      alert("Zadej nazev ukol.");
+  $scope.cellClass = function (row, cell) {
+    if (remove($scope.board, row, cell)) {
+      return "die";
     }
+    if (newLife($scope.board, row, cell)) {
+      return "new";
+    }
+    return "";
   };
-}]);
 
-TODOAppControllers.controller("TaskEditCtrl", ["$http", "$rootScope", "$scope", "apiURL", function($http, $rootScope, $scope, apiURL) {
-	console.log("TaskEditCtrl");
-    $scope.updateTask = function() {
-      var ct = $rootScope.currentTask;
-      if(ct.title) {
-        var param = "update_id=" + ct.id
-                    + "&title=" + ct.title
-                    + "&description=" + ct.description
-                    + "&complete=" + (ct.complete ? "1" : "0");
-        console.log("VYPISU " + param);           
-        $http.post(apiURL, param).success(function(data) {
-          if (!data.success) {
-            alert("BYCHASA UKALDANI ");
-          }
-        });
-      } else {
-        alert("zadej nazev ukolu");
+  $scope.step = function (index) {
+    $scope.board = $scope.history[index];
+    $scope.history = $scope.history.slice(0, index);
+  };
+
+  $scope.toggle = function (row, cell) {
+    $scope.history = []; // Reset history as it is no longer accurate
+    $scope.board[row][cell] = !$scope.board[row][cell];
+  };
+
+  $scope.newGame(1);
+
+  function init(height, width) {
+
+    var board = [];
+    for (var h = 0 ; h < height ; h++) {
+      var row = [];
+      for (var w = 0 ; w < width ; w++) {
+        row.push(false);
       }
-    };
+      board.push(row);
+    }
+    return board;
+  }
+
+  $scope.runIt = function () {
+    if ($scope.runner) {
+      $interval.cancel($scope.runner);
+    }
+
+    $scope.runner = $interval(function () {
+      $scope.next();
+      }, $scope.runnerInterval);
+  };
+
+  $scope.stopIt = function () {
+    $interval.cancel($scope.runner);
+  };
+
+  function pattern1() { 
+	$scope.height = 19;
+	$scope.width = 54;
+    var board = init($scope.height, $scope.width);
+
+    board[1][1] = (true);
+    board[1][2] = (true);
+    board[2][2] = (true);
+    board[3][2] = (true);
+    board[4][3] = (true);
+    board[4][4] = (true);
+    board[3][4] = (true);
+    
+    board[17][1] = (true);
+    board[17][2] = (true);
+    board[16][2] = (true);
+    board[15][2] = (true);
+    board[14][3] = (true);
+    board[14][4] = (true);
+    board[15][4] = (true);
+    
+    board[1][52] = (true);
+    board[1][51] = (true);
+    board[2][51] = (true);
+    board[3][51] = (true);
+    board[4][50] = (true);
+    board[4][49] = (true);
+    board[3][49] = (true);
+    
+    board[17][52] = (true);
+    board[17][51] = (true);
+    board[16][51] = (true);
+    board[15][51] = (true);
+    board[14][50] = (true);
+    board[14][49] = (true);
+    board[15][49] = (true);
+    
+    board[15][26] = (true);
+    board[15][27] = (true);
+    board[14][27] = (true);
+    board[14][26] = (true);
+    
+    board[3][26] = (true);
+    board[3][27] = (true);
+    board[4][27] = (true);
+    board[4][26] = (true);
+    
+    board[5][12] = (true);
+    board[6][12] = (true);
+    board[7][12] = (true);
+    board[11][12] = (true);
+    board[12][12] = (true);
+    board[13][12] = (true);
+    board[5][13] = (true);
+    board[8][13] = (true);
+    board[10][13] = (true);
+    board[13][13] = (true);
+    board[5][14] = (true);
+    board[13][14] = (true);
+    board[6][16] = (true);
+    board[12][16] = (true);
+    board[7][17] = (true);
+    board[8][17] = (true);
+    board[10][17] = (true);
+    board[11][17] = (true);
+    
+    board[7][36] = (true);
+    board[7][37] = (true);
+    
+    board[11][36] = (true);
+    board[11][37] = (true);
+    
+    board[7][40] = (true);
+    board[7][41] = (true);
+    board[6][41] = (true);
+    board[5][41] = (true);
+    board[6][42] = (true);
+    board[5][40] = (true);
+    board[4][40] = (true);
+    
+    board[11][40] = (true);
+    board[11][41] = (true);
+    board[12][41] = (true);
+    board[13][41] = (true);
+    board[12][42] = (true);
+    board[13][40] = (true);
+    board[14][40] = (true);
+    
+    return board;
+  }
+
+  function computeNext(board) {
+    var newBoard = [];
+    for (var r = 0 ; r < board.length ; r++) {
+      var newRow = [];
+      for (var c = 0 ; c < board[r].length ; c++) {
+        newRow.push(remain(board, r, c) || newLife(board, r, c));
+      }
+      newBoard.push(newRow);
+    }
+    return newBoard;
+  }
+
+  function remain(board, row, cell) {
+    return valueAt(board, row, cell)
+        && countNeighbours(board, row, cell) >= 2
+        && countNeighbours(board, row, cell) <= 3;
+  }
+  function remove(board, row, cell) {
+    return valueAt(board, row, cell)
+        && (countNeighbours(board, row, cell) < 2
+        || countNeighbours(board, row, cell) > 3);
+  }
+  function newLife(board, row, cell) {
+    return !valueAt(board, row, cell)
+        && countNeighbours(board, row, cell) == 3;
+  }
+
+  function countNeighbours(board, row, cell) {
+    var n = 0;
+    n += valueAt(board, row-1, cell-1) ? 1 : 0;
+    n += valueAt(board, row-1, cell+0) ? 1 : 0;
+    n += valueAt(board, row-1, cell+1) ? 1 : 0;
+    n += valueAt(board, row+0, cell-1) ? 1 : 0;
+    n += valueAt(board, row+0, cell+1) ? 1 : 0;
+    n += valueAt(board, row+1, cell-1) ? 1 : 0;
+    n += valueAt(board, row+1, cell+0) ? 1 : 0;
+    n += valueAt(board, row+1, cell+1) ? 1 : 0;
+    return n;
+  }
+
+  function valueAt(board, row, cell) {
+    return (row >= 0 && row < board.length && cell >= 0  && cell < board[row].length && board[row][cell]);
+  }
+
 }]);
